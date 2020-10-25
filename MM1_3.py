c3e6165a25192e09832996b3679c1b4674cfffc5
dmberd <berdinsky@gmail.com>
2020-10-25 21:10:46 +0700

    Add files via upload

--- MM1_3.py
+++ MM1_3.py
@@ -6,28 +6,39 @@ import simpy
 import matplotlib
 import matplotlib.pyplot as plt
   
-random_seed = 101 
+random_seed = 11 
 running_time = 60*6
 interarrival_time = 5
 service_time = 5.5
 
+def update_total_time(increment):
+    customer_generator.total_waiting_time += increment
 
 class Customer(object): 
     
     def __init__(self,env,counter,identity,time): 
+        
         self.env=env 
         self.counter = counter 
         self.arrival_time=time
         self.customer_id = identity 
-
+        
+        # updating total waiting time: 
+        # note that it is possible that a customer does not enter the service 
+        
+        q = running_time - self.arrival_time
+        update_total_time(q)                               
+        
         env.process(self.service())
    
     def service(self): 
         counter_request = self.counter.request()
         yield counter_request
-       ###
-        global N 
-        N=self.customer_id        
+
+        #updating total waiting time
+        q = - (running_time - self.env.now)
+        update_total_time(q)                
+                        
        
         t = random.expovariate(1/ service_time)
         yield self.env.timeout(t) 
@@ -36,22 +47,26 @@ class Customer(object):
         
 def customer_generator(env,counter):  
     n=0
+    customer_generator.total_waiting_time = 0
     customer = Customer(env,counter,n,env.now)
     while True:       
         n = n + 1 
         t = random.expovariate (1 / interarrival_time) # delay between arrivals 
         yield env.timeout(t)
+        
+        customer_generator.last_customer_id = n  # we update the total number of arrived customers
+        
         customer = Customer(env,counter,n,env.now) 
          
+random.seed(random_seed)
+       
 
-random.seed(random_seed)       
-
-R=25
+R=50
 X = range(0,R)
 Y = []
 
 for i in X: 
-             
+                                   
     env=simpy.Environment()
 
     counter = simpy.Resource(env, capacity=1)
@@ -60,17 +75,9 @@ for i in X:
 
     env.run(running_time)
 
-    Y.append(N+1)
-
-S=0; 
-    
-for i in X: 
-    
-    S=S+Y[i]
-
-print('%0.2f'%(S/R))    
+    Y.append(customer_generator.total_waiting_time/(customer_generator.last_customer_id+1))
 
 plt.plot(X,Y,"-o")
-plt.ylabel("Total number of customers")
+plt.ylabel("Average waiting time")
 plt.xlabel("Simulation runs")
 plt.show()
