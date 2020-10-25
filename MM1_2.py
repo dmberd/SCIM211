019fa8ca364e468c25a2af7476d16be91838d6d4
dmberd <berdinsky@gmail.com>
2020-10-25 20:58:46 +0700

    Add files via upload

--- MM1_2.py
+++ MM1_2.py
@@ -1,5 +1,4 @@
-# M/M/1 model 
-# We did together in class 
+# M/M/1 model  
 
 
 import random 
@@ -10,21 +9,24 @@ running_time = 60*8
 interarrival_time = 5
 service_time = 5.5
 
-T=0
-N=0
+def update_total_time(increment):
+    customer_generator.total_waiting_time += increment   
 
 class Customer(object): 
     def __init__(self,env,counter,identity,time): 
         self.env=env 
         self.counter = counter 
         self.arrival_time=time
-        self.customer_id = identity 
-        print('Customer%s arrived at %0.2f' %(self.customer_id,self.arrival_time)) # customer arrives
-        # updating total waiting time and the total number of customers
-        global T
-        T=T + (running_time - self.arrival_time)
-        global N 
-        N=self.customer_id       
+        self.customer_id = identity
+        
+        # customer arrives
+        print('Customer%s arrived at %0.2f' %(self.customer_id,self.arrival_time)) 
+        
+        # updating total waiting time: 
+        # note that it is possible that a customer does not enter the service     
+        q = running_time - self.arrival_time
+        update_total_time(q)
+        
         # start waiting the service
         env.process(self.service())
     def service(self): 
@@ -33,22 +35,32 @@ class Customer(object):
         print('Customer%s waited %0.2f minutes; started service at %0.2f' %(self.customer_id,
                                                                             self.env.now-self.arrival_time,self.env.now))
         # updating total waiting time
-        global T
-        T=T- (running_time - self.env.now)
+        q = - (running_time - self.env.now)
+        update_total_time(q)        
+        
+        #customer_generator.total_waiting_time = customer_generator.total_waiting_time - (running_time - self.env.now)
         # generating service time
         t = random.expovariate(1/ service_time)
         yield self.env.timeout(t) 
         print('Customer%s leaves the counter at %0.2f' %(self.customer_id,self.env.now))
         self.counter.release(counter_request)
-        
+          
 def customer_generator(env,counter):  
     n=0
+    customer_generator.total_waiting_time = 0 
     customer = Customer(env,counter,n,env.now)
     while True:       
-        n = n + 1 
-        t = random.expovariate (1 / interarrival_time) # delay between arrivals 
+        n = n + 1
+        
+        t = random.expovariate (1 / interarrival_time) # delay between arrivals            
+         
         yield env.timeout(t)
+        
+        customer_generator.last_customer_id = n  # we update the total number of arrived customers                                         
+                                                             
+                                                 
         customer = Customer(env,counter,n,env.now) 
+    
          
 
 random.seed(random_seed)       
@@ -63,6 +75,5 @@ env.process(customer_generator(env,counter))
 
 env.run(running_time)
 
-print('Total number of customers:%0.0f'%(N+1))
-print('Average waiting time:%0.2f'%(T/(N+1)))
-
+print('Total number of customers:%0.0f'%(customer_generator.last_customer_id+1))
+print('Average waiting time:%0.2f'%(customer_generator.total_waiting_time/(customer_generator.last_customer_id+1)))
